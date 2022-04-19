1373abf5480c037cd1f5fc05a0add98c58f5ccb3
Ivan Kuznetsov <me@jeiwan.ru>
2022-04-19 14:53:21 +0700

    Add Balancer

---
+++ src/Balancer.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {IERC20} from "./interfaces.sol";
+
+interface IBalancer {
+    function flashLoan(
+        address recipient,
+        address[] memory tokens,
+        uint256[] memory amounts,
+        bytes memory userData
+    ) external;
+}
+
+contract Balancer {
+    address immutable owner;
+
+    address constant balancerAddress =
+        0xBA12222222228d8Ba445958a75a0704d566BF2C8;
+
+    IBalancer constant balancer = IBalancer(balancerAddress);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function go(address[] calldata tokens, uint256[] calldata amounts) public {
+        if (msg.sender != owner) revert();
+
+        balancer.flashLoan(address(this), tokens, amounts, "");
+    }
+
+    function receiveFlashLoan(
+        IERC20[] calldata tokens,
+        uint256[] calldata amounts,
+        uint256[] calldata feeAmounts,
+        bytes calldata userData
+    ) public payable {
+        if (msg.sender != balancerAddress) revert();
+
+        IERC20(tokens[0]).transfer(balancerAddress, amounts[0]);
+    }
+}
---
+++ test/Balancer.t.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "ds-test/test.sol";
+import "../src/Balancer.sol";
+
+contract ContractTest is DSTest {
+    Balancer balancer;
+
+    address constant wethAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    function setUp() public {
+        balancer = new Balancer();
+    }
+
+    function testFlashLoan() public {
+        address[] memory assets = new address[](1);
+        assets[0] = wethAddress;
+
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = 1 ether;
+
+        for (uint256 i; i < 10; i++) {
+            balancer.go(assets, amounts);
+        }
+    }
+}
